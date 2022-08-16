913c7db6564cd7957d58d2e48005f3abd69fd682
cailloutr <caio.trocilo@gmail.com>
2022-08-16 14:39:24 -0300

    Implemented the list-detail layout with a SlidingPaneLayout

--- app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianListFragment.kt
@@ -19,10 +19,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.example.amphibians.R
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.amphibians.databinding.FragmentAmphibianListBinding
 
 
@@ -32,22 +32,57 @@ class AmphibianListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         val binding = FragmentAmphibianListBinding.inflate(inflater)
 
         // Call the view model method that calls the amphibians api
         viewModel.getAmphibiansList()
 
+
+        val slidingPaneLayout = binding.slidingPaneLayout
+        slidingPaneLayout.lockMode = SlidingPaneLayout.LOCK_MODE_LOCKED
+
+        // Connect the SlidingPaneLayout to the system back button.
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            AmphibianListOnBackPressedCallback(slidingPaneLayout)
+        )
+
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.recyclerView.adapter = AmphibianListAdapter(AmphibianListener { amphibian ->
             viewModel.onAmphibianClicked(amphibian)
-            findNavController()
-                .navigate(R.id.action_amphibianListFragment_to_amphibianDetailFragment)
+            binding.slidingPaneLayout.openPane()
         })
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+}
+
+class AmphibianListOnBackPressedCallback(
+    private val slidingPaneLayout: SlidingPaneLayout,
+) : OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+    SlidingPaneLayout.PanelSlideListener {
+
+    init {
+        slidingPaneLayout.addPanelSlideListener(this)
+    }
+
+    override fun handleOnBackPressed() {
+        slidingPaneLayout.closePane()
+    }
+
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+    }
+
+    override fun onPanelOpened(panel: View) {
+        isEnabled = true
+    }
+
+    override fun onPanelClosed(panel: View) {
+        isEnabled = false
     }
 }
